db61ee14a6d6b62ca46bdaa5ada291701a6795f7
Damjan15 <stamenovdamjan20@gmail.com>
2022-05-24 08:04:24 -0700

    Header component created, Brand component created

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,8 +1,29 @@
+import { google, slack, atlassain, dropbox, shopify } from "./imports"
 import "./brand.css"
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="Google" />
+      </div>
+
+      <div>
+        <img src={slack} alt="Slack" />
+      </div>
+
+      <div>
+        <img src={atlassain} alt="Atlassain" />
+      </div>
+
+      <div>
+        <img src={dropbox} alt="Dropbox" />
+      </div>
+
+      <div>
+        <img src={shopify} alt="Shopify" />
+      </div>
+    </div>
   )
 }
 
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,8 +1,30 @@
+import people from "../../assets/people.png"
+import ai from "../../assets/ai.png"
 import "./header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">Let's build something amazing with GPT-3 OpenAPI</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus facere adipisci ea sint sapiente natus numquam, tenetur nostrum nesciunt vitae earum quas? In asperiores perspiciatis eaque ipsam aliquid natus aliquam?</p>
+
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="People" />
+          <p>1,600 people requested access in last 24 hours</p>
+        </div>
+        </div>
+
+        <div className="gpt3__header-image">
+          <img src={ai} alt="Ai" />
+        </div>
+  
+    </div>
   )
 }
 
